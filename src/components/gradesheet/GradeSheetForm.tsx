4dc21c480c74db2b17fdb5cc4a1bf3cd155bd08b
Jagri Vai <jagrivai2@gmail.com>
2025-06-19 08:25:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/gradesheet/GradeSheetForm.tsx
+++ src/components/gradesheet/GradeSheetForm.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import type * as React from 'react';
+import * as React from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { gradeSheetFormSchema, type GradeSheetFormValues, GradeSheetExamTypes, type SubjectMarkInput } from '@/lib/types';
